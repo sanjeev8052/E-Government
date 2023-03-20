b8923c8a170d869d4aa5eb121f166024dd4e2ba7
Sujeet Maurya <109360865+only-sujeet@users.noreply.github.com>
2023-03-20 20:05:47 +0530

    npm init

--- E-Government/src/components/Admin/Tester.jsx
+++ E-Government/src/components/Admin/Tester.jsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
 
@@ -8,34 +7,26 @@ const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
-export default function Snakbar() {
+export default function Snakbar({message,type}) {
   const [open, setOpen] = React.useState(false);
 
-  const handleClick = () => {
-    setOpen(true);
-  };
+ React.useEffect(()=>{
+     setOpen(true);
+ },[message])
+
 
   const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-
     setOpen(false);
   };
-  return (
+ return (
     <Stack spacing={2} sx={{ width: '100%' }}>
-      <Button variant="outlined" onClick={handleClick}>
-        Open success snackbar
-      </Button>
-      <Snackbar open={open} autoHideDuration={2000}>
-        <Alert  severity="success" sx={{ width: '100%' }}>
-          This is a success message!
+     
+      <Snackbar open={open} autoHideDuration={2000} onClose={handleClose } anchorOrigin={{ vertical:"bottom" , horizontal:"center"}}>
+        <Alert  severity={type} sx={{ width: '100%' }}>
+         {message}
         </Alert>
       </Snackbar>
-      <Alert severity="error">This is an error message!</Alert>
-      <Alert severity="warning">This is a warning message!</Alert>
-      <Alert severity="info">This is an information message!</Alert>
-      <Alert severity="success">This is a success message!</Alert>
+     
     </Stack>
   );
-}
+}   
