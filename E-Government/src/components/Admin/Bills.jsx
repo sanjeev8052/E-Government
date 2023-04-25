61cb26123bac1edb90af4a83643ad24da2e68544
Sujeet Maurya <109360865+only-sujeet@users.noreply.github.com>
2023-04-25 23:23:57 +0530

    sujeet end

--- E-Government/src/components/Admin/Bills.jsx
+++ E-Government/src/components/Admin/Bills.jsx
@@ -10,7 +10,7 @@ import { useFormik } from 'formik'
 import { AddHomeTwoTone, AttachMoneyTwoTone, EditRoadTwoTone, LocalPhoneTwoTone, PeopleAltTwoTone } from '@mui/icons-material'
 import { useEffect } from 'react'
 import { Getbillcat } from '../../Action/Admin/Categories'
-import * as Yup from 'yup'
+import * as yup from 'yup'
 import { AddBill } from '../../Action/Admin/Bills'
 import { useNavigate } from 'react-router-dom'
 import AdminAuth from '../ProtectedRoute/AdminAuth'
@@ -28,33 +28,33 @@ const Bills = () => {
         area: "",
         amount: ""
     }
-    const validationSchema = Yup.object().shape({
-        billType: Yup.string().required("!! Please Select Bills Type"),
-        ownerName: Yup.string().required("!! Please Enter Owner Name"),
-        tenamentNo: Yup.string().required("!! Please Enter Tenament No."),
-        streetAddress: Yup.string().required("!! Please Enter streetAddress"),
-        area: Yup.string().required("!! Please Enter Area"),
-        amount: Yup.string().required("!! Please Enter Amount"),
+    const validationSchema = yup.object().shape({
+        billType: yup.string().required("!! Please Select Bills Type"),
+        ownerName: yup.string().required("!! Please Enter Owner Name"),
+        tenamentNo: yup.number().min(100000000000, ['Tenament no must have 12 digit']).max(999999999999, ['Tenament no must have 12 digit']).required(),
+        streetAddress: yup.string().required("!! Please Enter streetAddress"),
+        area: yup.string().required("!! Please Enter Area"),
+        amount: yup.string().required("!! Please Enter Amount"),
     })
     const colors = tokens(themes.palette.mode)
     const { values, touched, errors, handleBlur, handleChange, handleSubmit } = useFormik({
         initialValues: initialvalue,
         validationSchema: validationSchema,
         onSubmit: (values) => {
-           //console.log(values)
-           dispatch(AddBill(values))
-        navigate('/pbills')
+            console.log(values)
+            dispatch(AddBill(values))
+            navigate('/pbills')
         }
     })
     const styles = {
         Box: {
 
-            backgroundColor:  colors.primary[600],
+            backgroundColor: colors.primary[600],
             color: "black",
             width: "70%",
             margin: "5px auto",
             padding: "30px 30px",
-            borderRadius: "10px",height: "400px", overflow: "auto",
+            borderRadius: "10px", height: "400px", overflow: "auto",
             // boxShadow: "3px 3px 6px",
             // float: "left"
 
@@ -126,11 +126,9 @@ const Bills = () => {
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(Getbillcat())
-    }, [])
+    }, [dispatch])
 
-const reload = () => { 
-    dispatch(Getbillcat())
- }
+
 
     return (
         <div className='app'>
@@ -140,9 +138,7 @@ const reload = () => {
                 <Box m="15px" >
                     <Box display="flex" justifyContent="space-between" alignItems="center">
                         <Header title="Add Bills" subtitle="Welcome To Add Bills Page" />
-                        <Button variant="contained" color="success" onClick={reload}>
-                            Reload
-                        </Button>
+
                     </Box>
                     <Box justifyContent="center" alignItems="center" display="flex">
                         <Box sx={styles.Box} >
@@ -156,7 +152,9 @@ const reload = () => {
                                         name="billType"
                                         label="Bills Type"
                                         // value={dept}
+                                        value={values.billType}
                                         onChange={handleChange}
+                                        onBlur={handleBlur}
                                         InputLabelProps={{ style: { fontSize: 20 } }}
                                         placeholder="select Bill Type"
                                     >
@@ -165,8 +163,8 @@ const reload = () => {
                                                 <MenuItem value={data.billsType}>{data.billsType}</MenuItem>
                                             ))
                                         }
-                                      
-                                      
+
+
                                     </Select>
                                 </FormControl>
                                 {errors.billType && touched.billType ? (
@@ -176,11 +174,13 @@ const reload = () => {
                                     name="ownerName"
                                     label="Owner Name"
                                     sx={styles.textfield}
+                                    value={values.ownerName}
                                     type="text"
                                     variant='standard'
                                     placeholder='Enter Owner Name'
                                     color='secondary'
                                     onChange={handleChange}
+                                    onBlur={handleBlur}
                                     InputLabelProps={{ style: { fontSize: 20 } }}
                                     InputProps={{
                                         startAdornment: (<InputAdornment position="start"> <PeopleAltTwoTone color='secondary' /></InputAdornment>)
@@ -198,14 +198,16 @@ const reload = () => {
                                     variant='standard'
                                     placeholder='Enter Tenament No.'
                                     color='secondary'
+                                    value={values.tenamentNo}
                                     onChange={handleChange}
+                                    onBlur={handleBlur}
                                     InputLabelProps={{ style: { fontSize: 20 } }}
                                     InputProps={{
                                         startAdornment: (<InputAdornment position="start"> <AddHomeTwoTone color='secondary' /></InputAdornment>)
                                     }}
 
                                 />
-                                 {errors.tenamentNo && touched.tenamentNo? (
+                                {errors.tenamentNo && touched.tenamentNo ? (
                                     <Typography color="crimson"   >{errors.tenamentNo}</Typography>
                                 ) : null}
                                 <TextField
@@ -216,6 +218,8 @@ const reload = () => {
                                     variant='standard'
                                     placeholder='Enter Street Address'
                                     color='secondary'
+                                    value={values.streetAddress}
+                                    onBlur={handleBlur}
                                     onChange={handleChange}
                                     InputLabelProps={{ style: { fontSize: 20 } }}
                                     InputProps={{
@@ -223,7 +227,7 @@ const reload = () => {
                                     }}
 
                                 />
-                                 {errors.streetAddress && touched.streetAddress ? (
+                                {errors.streetAddress && touched.streetAddress ? (
                                     <Typography color="crimson"   >{errors.streetAddress}</Typography>
                                 ) : null}
                                 <TextField
@@ -235,13 +239,15 @@ const reload = () => {
                                     placeholder='Enter Area'
                                     color='secondary'
                                     onChange={handleChange}
+                                    value={values.area}
+                                    onBlur={handleBlur}
                                     InputLabelProps={{ style: { fontSize: 20 } }}
                                     InputProps={{
                                         startAdornment: (<InputAdornment position="start"> <EditRoadTwoTone color='secondary' /></InputAdornment>)
                                     }}
 
                                 />
-                                 {errors.area && touched.area ? (
+                                {errors.area && touched.area ? (
                                     <Typography color="crimson"   >{errors.area}</Typography>
                                 ) : null}
                                 <TextField
@@ -252,14 +258,16 @@ const reload = () => {
                                     variant='standard'
                                     placeholder='Enter Amount'
                                     color='secondary'
+                                    value={values.amount}
                                     onChange={handleChange}
+                                    onBlur={handleBlur}
                                     InputLabelProps={{ style: { fontSize: 20 } }}
                                     InputProps={{
                                         startAdornment: (<InputAdornment position="start"> <AttachMoneyTwoTone color='secondary' /></InputAdornment>)
                                     }}
 
                                 />
-                                 {errors.amount && touched.amount ? (
+                                {errors.amount && touched.amount ? (
                                     <Typography color="crimson"   >{errors.amount}</Typography>
                                 ) : null}
                                 <Button type="submit" sx={styles.btn} variant="contained" color="secondary" >
@@ -274,4 +282,4 @@ const reload = () => {
     )
 }
 
-export default  AdminAuth(Bills)
+export default AdminAuth(Bills)
